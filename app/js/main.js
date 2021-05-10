cb8b6aa2e104b3d76d1e6a60cd14aa202113a9df
ylytvyn <julia.lytvyn.wonder@gmail.com>
2021-05-10 05:50:17 +0300

    complete desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
-(function($){
-	$(document).ready(function() {
-		// Code
+// (function($){
+// 	$(document).ready(function() {
+// 		// Code
 		
-	});
-})(jQuery);
+// 	});
+// })(jQuery);
+
+const swiper = new Swiper('.swiper-container', {
+	direction: 'horizontal',
+	slidesPerView: 1,
+	loop: true,
+  
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+  
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	}
+});
